d69cfcafb9f69147da39608b5efd088dd330dff8
Beata G <beatag@Beatas-MacBook-Air.local>
2018-10-28 18:16:58 +0000

    Second commit

--- catalog.php
+++ catalog.php
@@ -1,20 +1,45 @@
 <?php 
+$catalog = array();
+$catalog[101]= "Design Patterns";
+$catalog[201]= "Beethoven";
+$catalog[301]= "Forrest Gump";
+$catalog[102]="Clean Code";
+
+
 $pageTitle = "Full Catalog";
+$section = null;
 
 if (isset($_GET["cat"])) {
     if ($_GET["cat"] == "books") {
         $pageTitle = "Books";
+        $section = "books";
     } else if ($_GET["cat"] == "movies") {
         $pageTitle = "Movies";
+        $section = "movies";
     } else if ($_GET["cat"] == "music") {
         $pageTitle = "Music";
+        $section = "music";
     }
 }
 
 include("inc/header.php"); ?>
 
 <div class="section catalog page">
-    <h1><?php echo $pageTitle; ?></h1>
+    <div class = "wrapper">
+            <h1><?php echo $pageTitle; ?></h1>
+        <ul>
+            <?php 
+            foreach ($catalog as $item){
+                echo "<li>" .$item ."</li>";
+                
+            }
+
+            ?>
+        
+        
+        </ul>
+    </div>
+    
 </div>
 
 <?php include("inc/footer.php"); ?>
