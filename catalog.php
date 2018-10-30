c1a91637fff2f7b015f03ebe7ce74ec70045f522
Beata G <beatag@Beatas-MacBook-Air.local>
2018-10-30 14:14:47 +0000

    Added data.php file

--- catalog.php
+++ catalog.php
@@ -1,9 +1,6 @@
 <?php 
-$catalog = array();
-$catalog[101]= "Design Patterns";
-$catalog[201]= "Beethoven";
-$catalog[301]= "Forrest Gump";
-$catalog[102]="Clean Code";
+
+include("inc/data.php");
 
 
 $pageTitle = "Full Catalog";
